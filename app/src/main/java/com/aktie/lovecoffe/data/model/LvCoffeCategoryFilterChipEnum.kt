b0955978a3b3b7a78cefe7962afcee3b7deb06f0
Sandrolaxx <sandro--ramos@hotmail.com>
2024-12-08 22:31:54 -0300

    fix component

--- app/src/main/java/com/aktie/lovecoffe/data/model/LvCoffeCategoryFilterChipEnum.kt
+++ app/src/main/java/com/aktie/lovecoffe/data/model/LvCoffeCategoryFilterChipEnum.kt
@@ -13,12 +13,12 @@ enum class LvCoffeCategoryFilterChipEnum(
     SUPERMERCADO(description = "Supermercado", icon = R.drawable.ic_shopping_cart),
     ENTRETERIMENTO(description = "Cinema", icon = R.drawable.ic_movie),
     FARMACIA(description = "Farmácia", icon = R.drawable.ic_first_aid_kit),
-    COMBUSTIVEL(description = "Combistível", icon = R.drawable.ic_gas_station),
+    COMBUSTIVEL(description = "Combustível", icon = R.drawable.ic_gas_station),
     PADARIA(description = "Padaria", icon = R.drawable.ic_bakery);
 
     companion object {
         fun fromDescription(description: String): LvCoffeCategoryFilterChipEnum? {
-            return entries.find { it.description == description }
+            return values().find { it.description == description }
         }
     }
 }
