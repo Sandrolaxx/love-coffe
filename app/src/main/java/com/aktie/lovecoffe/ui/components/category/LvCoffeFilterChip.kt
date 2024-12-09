a7d2b22d7c3d596712236db675ecc87d31b7f5b4
Sandrolaxx <sandro--ramos@hotmail.com>
2024-12-09 13:51:57 -0300

    update compose

--- app/src/main/java/com/aktie/lovecoffe/ui/components/category/LvCoffeFilterChip.kt
+++ app/src/main/java/com/aktie/lovecoffe/ui/components/category/LvCoffeFilterChip.kt
@@ -1,8 +1,15 @@
 package com.aktie.lovecoffe.ui.components.category
 
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FilterChip
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.aktie.lovecoffe.data.model.LvCoffeCategory
+import com.aktie.lovecoffe.ui.theme.Gray400
+import com.aktie.lovecoffe.ui.theme.Typography
 
 @Composable
 fun LvCoffeCategoryFilterChip(
@@ -11,5 +18,19 @@ fun LvCoffeCategoryFilterChip(
     onClick: (isSelected: Boolean) -> Unit,
     category: LvCoffeCategory
 ) {
-
+    FilterChip(
+        modifier = modifier
+            .padding(2.dp)
+            .heightIn(min = 36.dp),
+        selected = isSelected,
+        elevation = Chip
+        onClick = { onClick(isSelected) },
+        label = {
+            Text(
+                text = category.name,
+                style = Typography.body2,
+                color = if (isSelected) Color.White else Gray400
+            )
+        }
+    )
 }
